fec3a33036734c610ed6f86aa0709b6028535315
sbagher <sbagher@ut.ac.ir>
2023-06-08 14:56:18 +0330

    Update Prj01.py

--- Chapter08/Prj01.py
+++ Chapter08/Prj01.py
@@ -16,22 +16,22 @@ def train(X, y, n_hidden, learning_rate, n_iter):
     W2 = np.random.randn(n_hidden, 1)
     b2 = np.zeros((1, 1))
     for i in range(1, n_iter+1):
-    Z2 = np.matmul(X, W1) + b1
-    A2 = sigmoid(Z2)
-    Z3 = np.matmul(A2, W2) + b2
-    A3 = Z3
-    dZ3 = A3 - y
-    dW2 = np.matmul(A2.T, dZ3)
-    db2 = np.sum(dZ3, axis=0, keepdims=True)
-    dZ2 = np.matmul(dZ3, W2.T) * sigmoid_derivative(Z2)
-    dW1 = np.matmul(X.T, dZ2)
-    db1 = np.sum(dZ2, axis=0)
-    W2 = W2 - learning_rate * dW2 / m
-    b2 = b2 - learning_rate * db2 / m
-    W1 = W1 - learning_rate * dW1 / m
-    b1 = b1 - learning_rate * db1 / m
-    if i % 100 == 0:
-    cost = np.mean((y - A3) ** 2)
-    print('Iteration %i, training loss: %f' %(i, cost))
+        Z2 = np.matmul(X, W1) + b1
+        A2 = sigmoid(Z2)
+        Z3 = np.matmul(A2, W2) + b2
+        A3 = Z3
+        dZ3 = A3 - y
+        dW2 = np.matmul(A2.T, dZ3)
+        db2 = np.sum(dZ3, axis=0, keepdims=True)
+        dZ2 = np.matmul(dZ3, W2.T) * sigmoid_derivative(Z2)
+        dW1 = np.matmul(X.T, dZ2)
+        db1 = np.sum(dZ2, axis=0)
+        W2 = W2 - learning_rate * dW2 / m
+        b2 = b2 - learning_rate * db2 / m
+        W1 = W1 - learning_rate * dW1 / m
+        b1 = b1 - learning_rate * db1 / m
+        if i % 100 == 0:
+            cost = np.mean((y - A3) ** 2)
+            print('Iteration %i, training loss: %f' %(i, cost))
     model = {'W1':W1, 'b1': b1, 'W2': W2, 'b2': b2}
     return model
