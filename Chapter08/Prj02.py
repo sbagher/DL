1d990571401253dfac9e997bbb2b2e5ab2f964e5
sbagher <sbagher@ut.ac.ir>
2023-06-09 15:45:07 +0330

    Update Prj02.py

--- Chapter08/Prj02.py
+++ Chapter08/Prj02.py
@@ -7,8 +7,8 @@ from sklearn import preprocessing
 from sklearn.neural_network import MLPRegressor
 import pandas as pd
 
-# data_url = "http://lib.stat.cmu.edu/datasets/boston"
-data_url = "boston"
+data_url = "http://lib.stat.cmu.edu/datasets/boston"
+# data_url = "boston"
 raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
 data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
 target = raw_df.values[1::2, 2]
