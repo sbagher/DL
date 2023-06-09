284c0633a67b51a695df4ebbfea68d0ffec9b435
sbagher <sbagher@ut.ac.ir>
2023-06-09 18:10:26 +0330

    Update Prj04.py

--- Chapter08/Prj04.py
+++ Chapter08/Prj04.py
@@ -3,6 +3,3 @@
 # Class: Deep Learning
 # Assignment: Project: 04, Chapter: 08, Book: "Python Machine Learning By Example"
 
-import quandl
-
-mydata = quandl.get("YAHOO/INDEX_DJI", start_date="2005-12-01", end_date="2005-12-05")
