c525ee8240ced7921143113e5b1e1dbaad3289c6
sbagher <sbagher@ut.ac.ir>
2023-06-09 08:12:43 +0330

    Update Prj01.py

--- Chapter08/Prj01.py
+++ Chapter08/Prj01.py
@@ -42,8 +42,9 @@ def train(X, y, n_hidden, learning_rate, n_iter):
 
 
 
-
-raw_df = pd.read_csv("boston.csv", sep="\s+", skiprows=22, header=None)
+data_url = "http://lib.stat.cmu.edu/datasets/boston"
+data_url = "boston"
+raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
 data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
 target = raw_df.values[1::2, 2]
 
