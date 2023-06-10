6e439e81eb8b1b6164c2425819ef4d21dc9524ad
sbagher <sbagher@ut.ac.ir>
2023-06-10 17:45:28 +0330

    Update Prj04.py

--- Chapter08/Prj04.py
+++ Chapter08/Prj04.py
@@ -4,13 +4,6 @@
 # Assignment: Project: 04, Chapter: 08, Book: "Python Machine Learning By Example"
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-import numpy as np
-from sklearn.linear_model import SGDRegressor
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.svm import SVR
-import matplotlib.pyplot as plt
 
 def add_original_feature(df, df_new):
     df_new['open'] = df['Open']
@@ -98,70 +91,3 @@ end_test = '2022-12-31'
 data_test = data.loc[start_test:end_test]
 X_test = data_test.drop('close', axis=1).values
 y_test = data_test['close'].values
-print(X_train.shape)
-print(y_train.shape)
-print(X_test.shape)
-print(y_test.shape)
-
-scaler = StandardScaler()
-X_scaled_train = scaler.fit_transform(X_train)
-X_scaled_test = scaler.transform(X_test)
-
-param_grid = {
-    "alpha": [1e-4, 3e-4, 1e-3],
-    "eta0": [0.01, 0.03, 0.1],
-    }
-lr = SGDRegressor(penalty='l2', max_iter=1000, random_state=42)
-grid_search = GridSearchCV(lr, param_grid, cv=5, scoring='r2')
-grid_search.fit(X_scaled_train, y_train)
-
-print(grid_search.best_params_)
-lr_best = grid_search.best_estimator_
-predictions_lr = lr_best.predict(X_scaled_test)
-
-print(f'MSE: {mean_squared_error(y_test, predictions_lr):.3f}')
-print(f'MAE: {mean_absolute_error(y_test, predictions_lr):.3f}')
-print(f'R^2: {r2_score(y_test, predictions_lr):.3f}')
-
-param_grid = {
-    'max_depth': [30, 50],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [3, 5]
-    }
-rf = RandomForestRegressor(n_estimators=100, n_jobs=-1, max_features=1.0, random_state=42)
-grid_search = GridSearchCV(rf, param_grid, cv=5, scoring='r2', n_jobs=-1)
-grid_search.fit(X_train, y_train)
-
-print(grid_search.best_params_)
-rf_best = grid_search.best_estimator_
-predictions_rf = rf_best.predict(X_test)
-
-print(f'MSE: {mean_squared_error(y_test, predictions_rf):.3f}')
-print(f'MAE: {mean_absolute_error(y_test, predictions_rf):.3f}')
-print(f'R^2: {r2_score(y_test, predictions_rf):.3f}')
-
-param_grid = [
-    {'kernel': ['linear'], 'C': [100, 300, 500], 'epsilon': [0.00003, 0.0001]},
-    {'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],'C': [10, 100, 1000], 'epsilon': [0.00003, 0.0001]}
-]
-
-svr = SVR()
-grid_search = GridSearchCV(svr, param_grid, cv=5, scoring='r2')
-grid_search.fit(X_scaled_train, y_train)
-
-print(grid_search.best_params_)
-svr_best = grid_search.best_estimator_
-predictions_svr = svr_best.predict(X_scaled_test)
-print(f'MSE: {mean_squared_error(y_test, predictions_svr):.3f}')
-print(f'MAE: {mean_absolute_error(y_test, predictions_svr):.3f}')
-print(f'R^2: {r2_score(y_test, predictions_svr):.3f}')
-
-plt.plot(data_test.index, y_test, c='k')
-plt.plot(data_test.index, predictions_lr, c='b')
-plt.plot(data_test.index, predictions_rf, c='r')
-plt.plot(data_test.index, predictions_svr, c='g')
-plt.xticks(range(0, 252, 10), rotation=60)
-plt.xlabel('Date')
-plt.ylabel('Close price')
-plt.legend(['Truth', 'Linear regression', 'Random Forest', 'SVR'])
-plt.show()
