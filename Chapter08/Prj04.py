534f51eb0f8428f03ebc814f1a1240a9c69f5e84
sbagher <sbagher@ut.ac.ir>
2023-06-10 18:19:46 +0330

    Update Prj04.py

--- Chapter08/Prj04.py
+++ Chapter08/Prj04.py
@@ -6,8 +6,10 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
 from tensorflow import keras
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
+from tensorboard.plugins.hparams import api as hp
 
 def add_original_feature(df, df_new):
     df_new['open'] = df['Open']
@@ -101,7 +103,6 @@ scaler = StandardScaler()
 X_scaled_train = scaler.fit_transform(X_train)
 X_scaled_test = scaler.transform(X_test)
 
-
 model = Sequential([
     Dense(units=32, activation='relu'),
     Dense(units=1)
@@ -109,3 +110,32 @@ model = Sequential([
 model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
 model.fit(X_scaled_train, y_train, epochs=100, verbose=True)
 
+predictions = model.predict(X_scaled_test)
+
+print(f'MSE: {mean_squared_error(y_test, predictions):.3f}')
+print(f'MAE: {mean_absolute_error(y_test, predictions):.3f}')
+print(f'R^2: {r2_score(y_test, predictions):.3f}')
+
+HP_HIDDEN = hp.HParam('hidden_size', hp.Discrete([64, 32, 16]))
+HP_EPOCHS = hp.HParam('epochs', hp.Discrete([300, 1000]))
+HP_LEARNING_RATE = hp.HParam('learning_rate', hp.RealInterval(0.01, 0.4))
+
+def train_test_model(hparams, logdir):
+    model = Sequential([
+        Dense(units=hparams[HP_HIDDEN], activation='relu'),
+        Dense(units=1)
+        ])
+    model.compile(loss='mean_squared_error',
+                  optimizer=tf.keras.optimizers.Adam(hparams[HP_LEARNING_RATE]),
+                  metrics=['mean_squared_error']
+                  )
+    model.fit(X_scaled_train, y_train,
+              validation_data=(X_scaled_test, y_test),
+              epochs=hparams[HP_EPOCHS], verbose=False,
+              callbacks=[tf.keras.callbacks.TensorBoard(logdir),hp.KerasCallback(logdir, hparams),
+                         tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0,patience=200, verbose=0,mode='auto',)],
+                         )
+    _, mse = model.evaluate(X_scaled_test, y_test)
+    pred = model.predict(X_scaled_test)
+    r2 = r2_score(y_test, pred)
+    return mse, r2
