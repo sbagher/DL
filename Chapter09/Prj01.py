96951a13c26e6c4f9b1714a50860b287adbf6c4e
sbagher <sbagher@ut.ac.ir>
2023-06-16 16:50:40 +0330

    Update Prj01.py

--- Chapter09/Prj01.py
+++ Chapter09/Prj01.py
@@ -1,4 +1,4 @@
 # Name: Saeed Baghershahi
 # Student Number: 102501002
 # Class: Deep Learning
-# Assignment: Project: 01, Chapter: 08, Book: "Python Machine Learning By Example"
+# Assignment: Project: 01, Chapter: 09, Book: "Python Machine Learning By Example"
