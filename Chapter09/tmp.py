1d0dc7798124e9d9344ec962a4fba8e94d14c3b9
sbagher <sbagher@ut.ac.ir>
2023-06-16 20:43:59 +0330

    Update tmp.py

--- Chapter09/tmp.py
+++ Chapter09/tmp.py
@@ -18,3 +18,17 @@ print(word_tokenize(sent2))
 
 import spacy
 nlp = spacy.load('en_core_web_sm')
+tokens2 = nlp(sent2)
+print([token.text for token in tokens2])
+
+from nltk.tokenize import sent_tokenize
+print(sent_tokenize(sent))
+
+import nltk
+tokens = word_tokenize(sent)
+print(nltk.pos_tag(tokens))
+
+nltk.help.upenn_tagset('PRP')
+nltk.help.upenn_tagset('VBP')
+
+print([(token.text, token.pos_) for token in tokens2])
