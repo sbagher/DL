7c22538a659511469151c0abc741511d872e4a34
sbagher <sbagher@ut.ac.ir>
2023-06-16 17:21:49 +0330

    Update Prj01.py

--- Chapter09/Prj01.py
+++ Chapter09/Prj01.py
@@ -2,3 +2,6 @@
 # Student Number: 102501002
 # Class: Deep Learning
 # Assignment: Project: 01, Chapter: 09, Book: "Python Machine Learning By Example"
+
+from nltk.corpus import names
+print(names.words()[:10])
