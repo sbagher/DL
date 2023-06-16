6d571d25f23fdf0905d7bf616b05bac215082eaf
sbagher <sbagher@ut.ac.ir>
2023-06-16 22:25:27 +0330

    Update Prj01.py

--- Chapter09/Prj01.py
+++ Chapter09/Prj01.py
@@ -4,4 +4,6 @@
 # Assignment: Project: 01, Chapter: 09, Book: "Python Machine Learning By Example"
 
 from sklearn.datasets import fetch_20newsgroups
-roups = fetch_20newsgroups()
+groups = fetch_20newsgroups()
+
+print (groups.keys())
