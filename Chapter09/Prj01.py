43eb49ae0106a90fbde8762ca72a8b5b392b612b
sbagher <sbagher@ut.ac.ir>
2023-06-16 20:34:14 +0330

    Update Prj01.py

--- Chapter09/Prj01.py
+++ Chapter09/Prj01.py
@@ -6,3 +6,13 @@
 from nltk.corpus import names
 print(names.words()[:10])
 print(len(names.words()))
+
+from nltk.tokenize import word_tokenize
+sent = '''I am reading a book.
+          It is Python Machine Learning By Example, 
+          3rd edition.'''
+print(word_tokenize(sent))
+
+sent2 = 'I have been to U.K. and U.S.A.'
+print(word_tokenize(sent2))
+
