1df71b933272edb87cebe9cee38aff8a84c81d75
sbagher <sbagher@ut.ac.ir>
2023-06-22 16:35:24 +0330

    Update tmp.py

--- Chapter09/tmp.py
+++ Chapter09/tmp.py
@@ -1,7 +1,7 @@
 # Name: Saeed Baghershahi
 # Student Number: 102501002
 # Class: Deep Learning
-# Assignment: Project: 01, Chapter: 09, Book: "Python Machine Learning By Example"
+# Assignment: Project: 00, Chapter: 09, Book: "Python Machine Learning By Example"
 
 from nltk.corpus import names
 print(names.words()[:10])
