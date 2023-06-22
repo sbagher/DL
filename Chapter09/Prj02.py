bea64083cb1a4c81e81447143556f61c0d4dfa34
sbagher <sbagher@ut.ac.ir>
2023-06-22 16:36:37 +0330

    Update Prj02.py

--- Chapter09/Prj02.py
+++ Chapter09/Prj02.py
@@ -4,25 +4,9 @@
 # Assignment: Project: 02, Chapter: 09, Book: "Python Machine Learning By Example"
 
 from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 groups = fetch_20newsgroups()
 
-print (groups.keys())
-
-print (groups['target_names'])
-
-print (groups.target)
-print (np.unique(groups.target))
-
-
-sns.distplot(groups.target)
-plt.show()
-
-print (groups.data[0])
-
-print (groups.target[0])
-
-print (groups.target_names[groups.target[0]])
