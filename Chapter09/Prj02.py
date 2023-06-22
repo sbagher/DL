b7971ac6c921c0f8de9cb9e0ddae407e949f8069
sbagher <sbagher@ut.ac.ir>
2023-06-22 16:35:01 +0330

    Create Prj02.py

---
+++ Chapter09/Prj02.py
@@ -0,0 +1,28 @@
+# Name: Saeed Baghershahi
+# Student Number: 102501002
+# Class: Deep Learning
+# Assignment: Project: 01, Chapter: 09, Book: "Python Machine Learning By Example"
+
+from sklearn.datasets import fetch_20newsgroups
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+groups = fetch_20newsgroups()
+
+print (groups.keys())
+
+print (groups['target_names'])
+
+print (groups.target)
+print (np.unique(groups.target))
+
+
+sns.distplot(groups.target)
+plt.show()
+
+print (groups.data[0])
+
+print (groups.target[0])
+
+print (groups.target_names[groups.target[0]])
