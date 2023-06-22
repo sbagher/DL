ca36e3355a35327d98b7f67dfa795ea881914b36
sbagher <sbagher@ut.ac.ir>
2023-06-22 11:11:38 +0330

    Update Prj01.py

--- Chapter09/Prj01.py
+++ Chapter09/Prj01.py
@@ -4,8 +4,25 @@
 # Assignment: Project: 01, Chapter: 09, Book: "Python Machine Learning By Example"
 
 from sklearn.datasets import fetch_20newsgroups
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 groups = fetch_20newsgroups()
 
 print (groups.keys())
 
 print (groups['target_names'])
+
+print (groups.target)
+print (np.unique(groups.target))
+
+
+sns.distplot(groups.target)
+plt.show()
+
+print (groups.data[0])
+
+print (groups.target[0])
+
+print (groups.target_names[groups.target[0]])
