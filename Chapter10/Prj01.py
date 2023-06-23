58ffc5779ab8714c0a044932dd156a5a96cd8b98
sbagher <sbagher@ut.ac.ir>
2023-06-23 16:38:21 +0330

    Update Prj01.py

--- Chapter10/Prj01.py
+++ Chapter10/Prj01.py
@@ -2,3 +2,8 @@
 # Student Number: 102501002
 # Class: Deep Learning
 # Assignment: Project: 01, Chapter: 10, Book: "Python Machine Learning By Example"
+
+from sklearn import datasets
+iris = datasets.load_iris()
+X = iris.data[:, 2:4]
+y = iris.target
