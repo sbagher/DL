27834a4657e118124dde4625f317089df2eb1baf
sbagher <sbagher@ut.ac.ir>
2023-06-23 11:05:16 +0330

    Update Prj03.py

--- Chapter09/Prj03.py
+++ Chapter09/Prj03.py
@@ -5,31 +5,21 @@
 
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem import WordNetLemmatizer
 import numpy as np
 import matplotlib.pyplot as plt
 
 groups = fetch_20newsgroups()
 
-count_vector = CountVectorizer(max_features=500)
-data_count = count_vector.fit_transform(groups.data)
-print (data_count.astype)
-print(data_count[0].astype)
-print(data_count[0])
-print(data_count.toarray()[0])
-
-print(count_vector.get_feature_names_out())
-
+from nltk.corpus import names
+all_names = set(names.words())
+count_vector_sw = CountVectorizer(stop_words="english", max_features=500)
+lemmatizer = WordNetLemmatizer()
 data_cleaned = []
 for doc in groups.data:
-    doc_cleaned = ' '.join(word for word in doc.split() if word.isalpha())
+    doc = doc.lower()
+    doc_cleaned = ' '.join(lemmatizer.lemmatize(word)
+                           for word in doc.split()
+                           if word.isalpha() and word not in all_names)
     data_cleaned.append(doc_cleaned)
-
-data_count = count_vector.fit_transform(data_cleaned)
-print(count_vector.get_feature_names_out())
-
-from sklearn.feature_extraction import _stop_words
-print(_stop_words.ENGLISH_STOP_WORDS)
-
-count_vector_sw = CountVectorizer(stop_words="english", max_features=500)
-data_count = count_vector.fit_transform(data_cleaned)
-print(count_vector.get_feature_names_out())
+data_cleaned_count = count_vector_sw.fit_transform(data_cleaned)
