be8a63613aaa4692f225e4a62a0d6ba240c9d951
sbagher <sbagher@ut.ac.ir>
2023-06-30 19:36:59 +0330

    Update Prj04.py

--- Chapter10/Prj04.py
+++ Chapter10/Prj04.py
@@ -6,6 +6,7 @@
 from matplotlib import pyplot as plt
 from sklearn import datasets
 from sklearn.cluster import KMeans
+import numpy as np
 
 iris = datasets.load_iris()
 X = iris.data
@@ -13,3 +14,14 @@ y = iris.target
 k_list = list(range(1, 7))
 sse_list = [0] * len(k_list)
 
+for k_ind, k in enumerate(k_list):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(X)
+    clusters = kmeans.labels_
+    centroids = kmeans.cluster_centers_
+    sse = 0
+    for i in range(k):
+        cluster_i = np.where(clusters == i)
+        sse += np.linalg.norm(X[cluster_i] - centroids[i])
+    print('k={}, SSE={}'.format(k, sse))
+    sse_list[k_ind] = sse
