eb54dc81324dc9d71c0234899b2b58cf3971e0a0
sbagher <sbagher@ut.ac.ir>
2023-06-30 19:28:49 +0330

    Update Prj03.py

--- Chapter10/Prj03.py
+++ Chapter10/Prj03.py
@@ -1,9 +1,8 @@
 # Name: Saeed Baghershahi
 # Student Number: 102501002
 # Class: Deep Learning
-# Assignment: Project: 01, Chapter: 10, Book: "Python Machine Learning By Example"
+# Assignment: Project: 02, Chapter: 10, Book: "Python Machine Learning By Example"
 
-import numpy as np
 from matplotlib import pyplot as plt
 from sklearn import datasets
 from sklearn.cluster import KMeans
