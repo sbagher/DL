aec914d9b5b2b46689cfff62a68f5abca7b1db94
sbagher <sbagher@ut.ac.ir>
2023-06-30 19:34:43 +0330

    Update Prj04.py

--- Chapter10/Prj04.py
+++ Chapter10/Prj04.py
@@ -8,14 +8,8 @@ from sklearn import datasets
 from sklearn.cluster import KMeans
 
 iris = datasets.load_iris()
-X = iris.data[:, 2:4]
+X = iris.data
 y = iris.target
+k_list = list(range(1, 7))
+sse_list = [0] * len(k_list)
 
-kmeans_sk = KMeans(n_clusters=3, random_state=42)
-kmeans_sk.fit(X)
-clusters_sk = kmeans_sk.labels_
-centroids_sk = kmeans_sk.cluster_centers_
-
-plt.scatter(X[:, 0], X[:, 1], c=clusters_sk)
-plt.scatter(centroids_sk[:, 0], centroids_sk[:, 1], marker='*', s=200, c='#050505')
-plt.show()
