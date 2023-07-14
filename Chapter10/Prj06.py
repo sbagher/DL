af7e7c8af2a81da80ca2d6b4e228d898a901d30a
sbagher <sbagher@ut.ac.ir>
2023-07-14 18:43:46 +0330

    Update Prj06.py

--- Chapter10/Prj06.py
+++ Chapter10/Prj06.py
@@ -36,30 +36,3 @@ for doc in groups.data:
     doc_cleaned = ' '.join(lemmatizer.lemmatize(word) for word in doc.split() if word.isalpha() and word not in all_names)
     data_cleaned.append(doc_cleaned)
 
-count_vector = CountVectorizer(stop_words="english", max_features=None, max_df=0.5, min_df=2)
-data = count_vector.fit_transform(data_cleaned)
-
-k = 4
-kmeans = KMeans(n_clusters=k, random_state=42, init='random', n_init='auto')
-kmeans.fit(data)
-clusters = kmeans.labels_
-print(Counter(clusters))
-
-tfidf_vector = TfidfVectorizer(stop_words='english', max_features=None, max_df=0.5, min_df=2)
-data = tfidf_vector.fit_transform(data_cleaned)
-kmeans.fit(data)
-clusters = kmeans.labels_
-print(Counter(clusters))
-
-cluster_label = {i: labels[np.where(clusters == i)] for i in range(k)}
-terms = tfidf_vector.get_feature_names_out()
-centroids = kmeans.cluster_centers_
-for cluster, index_list in cluster_label.items():
-    counter = Counter(cluster_label[cluster])
-    print('cluster_{}: {} samples'.format(cluster, len(index_list)))
-    for label_index, count in sorted(counter.items(), key=lambda x: x[1], reverse=True):
-        print('{}: {} samples'.format(label_names[label_index], count))
-    print('Top 10 terms:')
-    for ind in centroids[cluster].argsort()[-10:]:
-        print(' %s' % terms[ind], end="")
-    print('\n')
