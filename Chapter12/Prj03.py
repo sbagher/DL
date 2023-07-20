6a56150e430e949bef7474cd99fe6277f6e9687d
sbagher <sbagher@ut.ac.ir>
2023-07-20 22:44:11 +0330

    Update Prj03.py

--- Chapter12/Prj03.py
+++ Chapter12/Prj03.py
@@ -7,68 +7,32 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers, models, losses
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img 
+
+def generate_plot_pics(datagen, original_img, save_prefix):
+    folder = 'aug_images'
+    i = 0
+    for batch in datagen.flow(original_img.reshape((1, 28, 28, 1)), batch_size=1, save_to_dir=folder, save_prefix=save_prefix, save_format='jpeg'):
+        i += 1
+        if i > 2:
+            break
+    plt.subplot(2, 2, 1, xticks=[],yticks=[])
+    plt.imshow(original_img)
+    plt.title("Original")
+    i = 1
+    for file in os.listdir(folder):
+        if file.startswith(save_prefix):
+            plt.subplot(2, 2, i + 1, xticks=[],yticks=[])
+            aug_img = load_img(folder + "/" + file)
+            plt.imshow(aug_img)
+            plt.title(f"Augmented {i}")
+            i += 1
+    plt.show()
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
-               'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-X_train = train_images.reshape((train_images.shape[0], 28, 28, 1))
-X_test = test_images.reshape((test_images.shape[0], 28, 28, 1))
-print(X_train.shape)
-
-tf.random.set_seed(42)
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
-model.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
-model.summary()
-model.fit(X_train, train_labels, validation_data=(X_test, test_labels), epochs=10)
-
-test_loss, test_acc = model.evaluate(X_test, test_labels, verbose=2)
-print('Accuracy on test set:', test_acc)
-
-predictions = model.predict(X_test)
-print(predictions[0])
-
-print('Predicted label for the first test sample: ', np.argmax(predictions[0]))
-print('True label for the first test sample: ', test_labels[0])
-
-def plot_image_prediction(i, images, predictions, labels, class_names):
-    plt.subplot(1,2,1)
-    plt.imshow(images[i], cmap=plt.cm.binary)
-    prediction = np.argmax(predictions[i])
-    color = 'blue' if prediction == labels[i] else 'red'
-    plt.title(f"{class_names[labels[i]]} (predicted {class_names[prediction]})", color=color)
-    plt.subplot(1,2,2)
-    plt.grid(False)
-    plt.xticks(range(10))
-    plot = plt.bar(range(10), predictions[i], color="#777777")
-    plt.ylim([0, 1])
-    plot[prediction].set_color('red')
-    plot[labels[i]].set_color('blue')
-    plt.show()
-
-plot_image_prediction(0, test_images, predictions, test_labels, class_names)
-
-filters, _ = model.layers[2].get_weights()
-
-f_min, f_max = filters.min(), filters.max()
-filters = (filters - f_min) / (f_max - f_min)
-
-n_filters = 16
-for i in range(n_filters):
-    filter = filters[:, :, :, i]
-    plt.subplot(4, 4, i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.imshow(filter[:, :, 0], cmap='gray')
-plt.show()
+datagen = ImageDataGenerator(horizontal_flip=True)
+generate_plot_pics(datagen, train_images[0], 'horizontal_flip')
 
