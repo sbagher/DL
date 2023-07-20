cd98b27e7d1b3dfa31e056b5dab96b2a56585acd
sbagher <sbagher@ut.ac.ir>
2023-07-20 11:41:08 +0330

    Update Prj01.py

--- Chapter12/Prj01.py
+++ Chapter12/Prj01.py
@@ -2,3 +2,7 @@
 # Student Number: 102501002
 # Class: Deep Learning
 # Assignment: Project: 01, Chapter: 12, Book: "Python Machine Learning By Example"
+
+import tensorflow as tf
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
