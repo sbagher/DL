fd762d82d1c7ec61f38592a399e33a3e961060e2
sbagher <sbagher@ut.ac.ir>
2023-07-22 18:28:00 +0330

    Update Prj04.py

--- Chapter12/Prj04.py
+++ Chapter12/Prj04.py
@@ -13,3 +13,11 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+X_train = train_images.reshape((train_images.shape[0], 28, 28, 1))
+X_test = test_images.reshape((test_images.shape[0], 28, 28, 1))
+
+n_small = 500
+X_train = X_train[:n_small]
+train_labels = train_labels[:n_small]
+print(X_train.shape)
+
