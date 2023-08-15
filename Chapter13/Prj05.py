2e024340a5565cfc4db609357f6261fe88c30cf9
sbagher <sbagher@ut.ac.ir>
2023-08-15 17:12:12 +0330

    Update Prj05.py

--- Chapter13/Prj05.py
+++ Chapter13/Prj05.py
@@ -6,6 +6,7 @@
 import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras import layers, models, losses, optimizers
+from tensorflow.keras.callbacks import Callback, ModelCheckpoint, EarlyStopping
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import matplotlib.pyplot as plt
 import numpy as np
@@ -46,3 +47,56 @@ for i in range(n_seq):
         index = char_to_index[char]
         y_sequence_ohe[j][index] = 1.
     Y[i] = y_sequence_ohe
+
+tf.random.set_seed(42)
+hidden_units = 700
+dropout = 0.4
+batch_size = 100
+n_epoch= 300
+model = models.Sequential()
+model.add(layers.LSTM(hidden_units, input_shape=(None, n_vocab), return_sequences=True, dropout=dropout))
+model.add(layers.LSTM(hidden_units, return_sequences=True, dropout=dropout))
+model.add(layers.TimeDistributed(layers.Dense(n_vocab, activation='softmax')))
+
+optimizer = optimizers.RMSprop(lr=0.001)
+model.compile(loss="categorical_crossentropy", optimizer=optimizer)
+print(model.summary())
+
+file_path = "weights/weights_epoch_{epoch:03d}_loss_{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(file_path, monitor='loss', verbose=1, save_best_only=True, mode='min')
+early_stop = EarlyStopping(monitor='loss', min_delta=0, patience=50, verbose=1, mode='min')
+
+def generate_text(model, gen_length, n_vocab, index_to_char):
+    """
+        Generating text using the RNN model
+        @param model: current RNN model
+        @param gen_length: number of characters we want to generate
+        @param n_vocab: number of unique characters
+        @param index_to_char: index to character mapping
+        @return: string of text generated
+    """
+    # Start with a randomly picked character
+    index = np.random.randint(n_vocab)
+    y_char = [index_to_char[index]]
+    X = np.zeros((1, gen_length, n_vocab))
+    for i in range(gen_length):
+        X[0, i, index] = 1.
+        indices = np.argmax(model.predict(X[:, max(0, i - seq_length -1):i + 1, :])[0], 1)
+        index = indices[-1]
+        y_char.append(index_to_char[index])
+    return ''.join(y_char)
+
+class ResultChecker(Callback):
+    def __init__(self, model, N, gen_length):
+        self.model = model
+        self.N = N
+        self.gen_length = gen_length
+
+    def on_epoch_end(self, epoch, logs={}):
+        if epoch % self.N == 0:
+            result = generate_text(self.model, self.gen_length, n_vocab, index_to_char)
+            print('\nMy War and Peace:\n' + result)
+
+result_checker = ResultChecker(model, 10, 500)
+
+model.fit(X, Y, batch_size=batch_size, verbose=1, epochs=n_epoch, callbacks=[result_checker, checkpoint, early_stop])
