6e352eb7aa0ca0dfedbb2db42dc1228d05d12414
sbagher <sbagher@ut.ac.ir>
2023-08-15 14:22:43 +0330

    Update Prj01.py

--- Chapter13/Prj01.py
+++ Chapter13/Prj01.py
@@ -7,3 +7,6 @@ import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras import layers, models, losses, optimizers
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+vocab_size = 5000
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=vocab_size)
