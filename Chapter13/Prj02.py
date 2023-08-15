acde7f4ed0ab7a720a7783cfcd1e18c480b72ebe
sbagher <sbagher@ut.ac.ir>
2023-08-15 15:48:59 +0330

    Update Prj02.py

--- Chapter13/Prj02.py
+++ Chapter13/Prj02.py
@@ -1,7 +1,7 @@
 # Name: Saeed Baghershahi
 # Student Number: 102501002
 # Class: Deep Learning
-# Assignment: Project: 01, Chapter: 13, Book: "Python Machine Learning By Example"
+# Assignment: Project: 02, Chapter: 13, Book: "Python Machine Learning By Example"
 
 import tensorflow as tf
 from tensorflow.keras.datasets import imdb
