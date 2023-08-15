331639281eab6f6049e95a6c102d986b55911a91
sbagher <sbagher@ut.ac.ir>
2023-08-15 16:44:23 +0330

    Update Prj04.py

--- Chapter13/Prj04.py
+++ Chapter13/Prj04.py
@@ -8,6 +8,7 @@ from tensorflow.keras.datasets import imdb
 from tensorflow.keras import layers, models, losses, optimizers
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import matplotlib.pyplot as plt
+import numpy as np
 
 training_file = 'warpeace_input.txt'
 raw_text = open(training_file, 'r').read()
@@ -29,3 +30,22 @@ print(chars)
 index_to_char = dict((i, c) for i, c in enumerate(chars))
 char_to_index = dict((c, i) for i, c in enumerate(chars))
 print(char_to_index)
+
+seq_length = 160
+n_seq = int(n_chars / seq_length)
+
+for i in range(n_seq):
+    x_sequence = raw_text[i * seq_length : (i + 1) * seq_length]
+    x_sequence_ohe = np.zeros((seq_length, n_vocab))
+    for j in range(seq_length):
+        char = x_sequence[j]
+        index = char_to_index[char]
+        x_sequence_ohe[j][index] = 1.
+    X[i] = x_sequence_ohe
+    y_sequence = raw_text[i * seq_length + 1 : (i + 1) * seq_length + 1]
+    y_sequence_ohe = np.zeros((seq_length, n_vocab))
+    for j in range(seq_length):
+        char = y_sequence[j]
+        index = char_to_index[char]
+        y_sequence_ohe[j][index] = 1.
+    Y[i] = y_sequence_ohe
