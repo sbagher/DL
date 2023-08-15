e3721ef7067a4d7fed50833ae3492ddae195142e
sbagher <sbagher@ut.ac.ir>
2023-08-15 14:21:28 +0330

    Update Prj01.py

--- Chapter13/Prj01.py
+++ Chapter13/Prj01.py
@@ -3,3 +3,7 @@
 # Class: Deep Learning
 # Assignment: Project: 01, Chapter: 13, Book: "Python Machine Learning By Example"
 
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras import layers, models, losses, optimizers
+from tensorflow.keras.preprocessing.sequence import pad_ sequences
