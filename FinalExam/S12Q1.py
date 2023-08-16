044db06e49b2193fc54c4f4f91cad10062c39129
sbagher <sbagher@ut.ac.ir>
2023-08-16 10:04:16 +0330

    Create S12Q1.py

---
+++ FinalExam/S12Q1.py
@@ -0,0 +1,43 @@
+# Name: Saeed Baghershahi
+# Student Number: 102501002
+# Class: Deep Learning
+# Assignment: Project: 04, Chapter: 12, Book: "Python Machine Learning By Example"
+
+import tensorflow as tf
+from tensorflow.keras import layers, models, losses
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+X_train = train_images.reshape((train_images.shape[0], 28, 28, 1))
+X_test = test_images.reshape((test_images.shape[0], 28, 28, 1))
+
+n_small = 500
+X_train = X_train[:n_small]
+train_labels = train_labels[:n_small]
+print(X_train.shape)
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+model.fit(X_train, train_labels, validation_data=(X_test, test_labels), epochs=20, batch_size=40)
+
+test_loss, test_acc = model.evaluate(X_test, test_labels, verbose=2)
+print('Accuracy on test set:', test_acc)
+
+datagen = ImageDataGenerator(height_shift_range=3, horizontal_flip=True)
+model_aug = tf.keras.models.clone_model(model)
+
+model_aug.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+train_generator = datagen.flow(X_train, train_labels, seed=42, batch_size=40)
+model_aug.fit(train_generator, epochs=50, validation_data=(X_test, test_labels))
+
+test_loss, test_acc = model_aug.evaluate(X_test, test_labels, verbose=2)
+print('Accuracy on test set:', test_acc)
