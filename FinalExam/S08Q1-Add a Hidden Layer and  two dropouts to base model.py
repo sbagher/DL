fbd38129254327b2f4b23520394aad260f462331
sbagher <sbagher@ut.ac.ir>
2023-08-17 10:59:48 +0330

    Update S08Q1-Add a Hidden Layer and  two dropouts to base model.py

--- FinalExam/S08Q1-Add a Hidden Layer and  two dropouts to base model.py
+++ FinalExam/S08Q1-Add a Hidden Layer and  two dropouts to base model.py
@@ -82,7 +82,10 @@ def generate_features(df):
 
 def train_test_model(hparams, logdir):
     model = Sequential([
-        Dense(units=hparams[HP_HIDDEN], activation='relu'),
+        Dense(units=hparams[HP_HIDDEN1], activation='relu'),
+        tf.keras.layers.Dropout(hparams[HP_DROPOUT1]),
+        Dense(units=hparams[HP_HIDDEN2], activation='relu'),
+        tf.keras.layers.Dropout(hparams[HP_DROPOUT2]),
         Dense(units=1)
         ])
     model.compile(loss='mean_squared_error',
@@ -103,7 +106,7 @@ def train_test_model(hparams, logdir):
 def run(hparams, logdir):
     with tf.summary.create_file_writer(logdir).as_default():
         hp.hparams_config(
-            hparams=[HP_HIDDEN, HP_EPOCHS, HP_LEARNING_RATE],
+            hparams=[HP_HIDDEN1, HP_HIDDEN2, HP_DROPOUT1, HP_DROPOUT2, HP_EPOCHS, HP_LEARNING_RATE],
             metrics=[hp.Metric('mean_squared_error', display_name='mse'), hp.Metric('r2', display_name='r2')],
             )
         mse, r2 = train_test_model(hparams, logdir)
@@ -111,9 +114,6 @@ def run(hparams, logdir):
         tf.summary.scalar('r2', r2, step=1)
 
 data_raw = pd.read_csv('19930101-20221231.csv', index_col='Date')
-
-data = generate_features(data_raw)
-print(data.round(decimals=3).head(5))
 
 data = generate_features(data_raw)
 start_train = '1993-01-01'
@@ -132,48 +132,30 @@ scaler = StandardScaler()
 X_scaled_train = scaler.fit_transform(X_train)
 X_scaled_test = scaler.transform(X_test)
 
-model = Sequential([
-    Dense(units=32, activation='relu'),
-    Dense(units=1)
-    ])
-model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
-model.fit(X_scaled_train, y_train, epochs=100, verbose=True)
-
-predictions = model.predict(X_scaled_test)[:, 0]
-
-print(f'MSE: {mean_squared_error(y_test, predictions):.3f}')
-print(f'MAE: {mean_absolute_error(y_test, predictions):.3f}')
-print(f'R^2: {r2_score(y_test, predictions):.3f}')
-
-HP_HIDDEN = hp.HParam('hidden_size', hp.Discrete([64, 32, 16]))
+HP_HIDDEN1 = hp.HParam('1st hidden layer', hp.Discrete([64, 32, 16]))
+HP_HIDDEN2 = hp.HParam('2nd hidden layer', hp.Discrete([64, 32, 16]))
+HP_DROPOUT1 = hp.HParam('dropout1', hp.RealInterval(0.2,0.4))
+HP_DROPOUT2 = hp.HParam('dropout2', hp.RealInterval(0.2,0.4))
 HP_EPOCHS = hp.HParam('epochs', hp.Discrete([300, 1000]))
 HP_LEARNING_RATE = hp.HParam('learning_rate', hp.RealInterval(0.01, 0.4))
-"""
-session_num = 0
-for hidden in HP_HIDDEN.domain.values:
-    for epochs in HP_EPOCHS.domain.values:
-        for learning_rate in tf.linspace(HP_LEARNING_RATE.domain.min_value, HP_LEARNING_RATE.domain.max_value, 5):
-            hparams = {
-                HP_HIDDEN: hidden, 
-                HP_EPOCHS: epochs, 
-                HP_LEARNING_RATE:float("%.2f"%float(learning_rate)),
-                }
-            run_name = "run-%d" % session_num
-            print('--- Starting trial: %s' % run_name)
-            print({h.name: hparams[h] for h in hparams})
-            run(hparams, 'logs/hparam_tuning/' + run_name)
-            session_num += 1
-"""
-model = Sequential([Dense(units=64, activation='relu'), Dense(units=1)])
-model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.21))
-model.fit(X_scaled_train, y_train, epochs=1000, verbose=False)
-predictions = model.predict(X_scaled_test)[:, 0]
 
-import matplotlib.pyplot as plt
-plt.plot(data_test.index, y_test, c='k')
-plt.plot(data_test.index, predictions, c='b')
-plt.xticks(range(0, 252, 10), rotation=60)
-plt.xlabel('Date')
-plt.ylabel('Close price')
-plt.legend(['Truth', 'Neural network prediction'])
-plt.show()
+session_num = 0
+for hidden1 in HP_HIDDEN1.domain.values:
+    for hidden2 in HP_HIDDEN2.domain.values:
+        for dropout1 in (HP_DROPOUT1.domain.min_value, HP_DROPOUT1.domain.max_value):
+            for dropout2 in (HP_DROPOUT2.domain.min_value, HP_DROPOUT2.domain.max_value):
+                for epochs in HP_EPOCHS.domain.values:
+                    for learning_rate in tf.linspace(HP_LEARNING_RATE.domain.min_value, HP_LEARNING_RATE.domain.max_value, 5):
+                        hparams = {
+                            HP_HIDDEN1: hidden1, 
+                            HP_HIDDEN2: hidden2, 
+                            HP_DROPOUT1: dropout1,
+                            HP_DROPOUT2: dropout2,
+                            HP_EPOCHS: epochs,
+                            HP_LEARNING_RATE:float("%.2f"%float(learning_rate)),
+                            }
+                        run_name = "run-%d" % session_num
+                        print('--- Starting trial: %s' % run_name)
+                        print({h.name: hparams[h] for h in hparams})
+                        run(hparams, 'logs8N5/hparam_tuning/' + run_name)
+                        session_num += 1
